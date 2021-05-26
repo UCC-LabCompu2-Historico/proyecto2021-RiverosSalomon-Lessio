026bff9e887d4e8d841213ad34209c564555677f
SantiagoRivSal <santiagosalomon2001@gmail.com>
2021-05-26 09:42:07 -0300

    Se mejoro sintaxis

--- operaciones/Codigo.js
+++ operaciones/Codigo.js
@@ -1,7 +1,7 @@
 function Cambio(){
-    var op,moneda,valor;
+    var operacion,moneda,valor;
     valor=document.getElementById("cambio").value;
-    op=document.getElementsByName("Operacion").value;
+    operacion=document.getElementsByName("Operacion").value;
     moneda=document.getElementsByName("moneda").value;
     if(isNaN(valor)){
         alert("Se ingreso un valor invalido");
