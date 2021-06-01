2d46c31102fb9b753dad76166ecc97a07c0a236d
SantiagoRivSal <santiagosalomon2001@gmail.com>
2021-06-01 13:36:12 -0300

    se crearon botones de calculo para mostrar los resultados y se comenzo con javascript

--- operaciones/Codigo.js
+++ operaciones/Codigo.js
@@ -1,9 +1,26 @@
-function Cambio(){
-    var operacion,moneda,valor;
-    valor=document.getElementById("cambio").value;
-    operacion=document.getElementsByName("Operacion").value;
-    moneda=document.getElementsByName("moneda").value;
-    if(isNaN(valor)){
-        alert("Se ingreso un valor invalido");
+function Cambio() {
+
+}
+
+function calcularPlazoFijo() {
+    var renovacion = document.getElementById("Renovacion_Meses").value;
+    var dinero = document.getElementById("Inversion_PF").value;
+    var banco = document.getElementById("option3");
+    var banco_Value = banco.options[banco.selectedIndex].value;
+    if (banco_Value == 1) {
+        var porcentaje_Mensual = 3.08;
     }
+    if (banco_Value == 2) {
+        var porcentaje_Mensual = 0.12;
+    }
+    var ganancia = (Number(dinero) * Number(porcentaje_Mensual))/100;
+    var Total= Number(dinero) + Number(ganancia);
+    var Total_Mensual= Number(Total) / Number(renovacion);
+    if(dinero>0 && renovacion>0){
+        document.getElementById("Total_PF").value= Total;
+    }
+}
+
+function calcularPrestamo() {
+
 }
